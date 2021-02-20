0f3e00d62eccdd2ef0b687ac39dd0f01c04aa52d
dg0397 <rgg0397@gmail.com>
2021-02-19 23:05:34 -0400

    fixing bug

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 
 let persons = [
   {
@@ -40,7 +40,7 @@ morgan.token("data", function (req, res) {
 });
 
 // MIDDLEWARES
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :data")
